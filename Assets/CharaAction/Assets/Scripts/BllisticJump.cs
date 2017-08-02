5e108ba7b0ddfeabd529e6ab7f8e4e9fef853bc9
VRzemi2017 <TCAVRzemi2017>
2017-08-02 17:48:33 +0900

    VR 対応、CharaAction, Enemy AI

---
+++ Assets/CharaAction/Assets/Scripts/BllisticJump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BllisticJump : MonoBehaviour {
+
+	public Transform target;
+	GameObject enemy;
+	Rigidbody rbody;
+
+	public float angle = 15f;
+	public float agroDis = 15f;
+	public float speed = 5f;
+
+	void Start(){
+		rbody = GetComponent<Rigidbody> ();
+		this.enemy = GameObject.Find ("Enemy");
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	Vector3 BallisticVelocityVector(Transform target, float angle){
+		Vector3 direction = target.position - transform.position;   // get target direction
+		float h = direction.y;    									// get height difference
+		direction.y = 0;                                            // remove height
+		float distance = direction.magnitude;                       // get horizontal distance
+		float a = angle * Mathf.Deg2Rad;                            // Convert angle to radians
+		direction.y = distance * Mathf.Tan(a);                      // Set direction to elevation angle
+		distance += h/Mathf.Tan(a);                                 // Correction for small height differences
+
+		// calculate velocity
+		float velocity = Mathf.Sqrt(distance * Physics.gravity.magnitude / Mathf.Sin(2*a) * speed); // def 2
+		return velocity * direction.normalized;
+	}
+
+	void FixedUpdate(){
+		if (Vector3.Distance (transform.position, target.transform.position) <= agroDis) {
+			transform.LookAt (target);
+			rbody.velocity = BallisticVelocityVector (target, angle);
+		}
+		Debug.Log ("Velocity:" + rbody.velocity);
+	}
+}
---
+++ Assets/CharaAction/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject[] enemy;
+	public Transform[] spawnPoint;
+
+	void Start () {
+		Spawn ();	
+	}
+
+	void Spawn(){
+		enemy[0] = Instantiate (enemy[0], spawnPoint[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		enemy[1] = Instantiate (enemy[1], spawnPoint[1].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+}
