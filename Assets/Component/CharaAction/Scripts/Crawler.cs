8be73deca94d3b47c0e267bb67e295c74827059f
dimas <suslovdd@gmail.com>
2017-08-19 15:38:48 +0900

    Added field w. path arrays and 7 enemies

--- Assets/Component/CharaAction/Scripts/Crawler.cs
+++ Assets/Component/CharaAction/Scripts/Crawler.cs
@@ -18,7 +18,6 @@ public class Crawler: MonoBehaviour {
 	private Transform[] targets; // target array
 
 	private int nowTarget; // current target
-	private Transform lastTarget;
 
 	// Time manager
 	private float timer;
@@ -27,7 +26,7 @@ public class Crawler: MonoBehaviour {
 	// Path loop
 	public bool patrolLoop;
 	private bool playerSpotted = false; // Player spotted
-	public float agroDis = 30.0f; // Player spot distance
+	public float agroDis = 10.0f; // Player attack distance
 
 	//Players
 	private Transform player1;
@@ -37,7 +36,7 @@ public class Crawler: MonoBehaviour {
 		rbody = GetComponent<Rigidbody> (); 						     // RBody get
 		myTransform = transform; 										 // Transform set
 		player1 = GameObject.FindGameObjectWithTag("Player1").transform; // find Player1 position
-	//	player2 = GameObject.FindGameObjectWithTag ("Player2").transform;// find Player2 position
+		player2 = GameObject.FindGameObjectWithTag ("Player2").transform;// find Player2 position
 	}
 		
 	private void Update(){
@@ -48,6 +47,13 @@ public class Crawler: MonoBehaviour {
 		} else if (patrolLoop) {
 			nowTarget = 0;
 		}
+
+		// Attack player within range 
+		/*if (Vector3.Distance (myTransform.position - player1.position) <= agroDis) {
+			AttackPlayer1 ();
+		} else if (Vector3.Distance (myTransform.position - player2.position) <= agroDis) {
+			AttackPlayer2 ();
+		} */
 	}
 		
 	// Move towards target
@@ -85,7 +91,7 @@ public class Crawler: MonoBehaviour {
 			moveSpeed = 2.0f;
 		}
 
-		// DEBUG LOG NEXT TARGET CHECK
+		// Rotate transform towards next target
 		if (other.tag == "SpeedUp" || other.tag == "SpeedNormal" || other.tag == "nowTarget" || other.tag == "Wait" ) {
 			myTransform.LookAt (targets [nowTarget + 1]);
 		}
@@ -97,5 +103,21 @@ public class Crawler: MonoBehaviour {
 			stopTimer = 0.5f;
 		}
 	}
+	/*	
+    // Player1 interaction
+    private void AttackPlayer1() {
+		moveSpeed = 5.0f;
+		Vector3 target = player1.position;
+		Vector3 dir = player1.position - myTransform.position;
+		myTransform.position = Vector3.MoveTowards (myTransform.position, target, moveSpeed * Time.deltaTime);
+    }
+
+	// Player2 interaction
+	private void AttackPlayer2() {
+		moveSpeed = 5.0f;
+		Vector3 target = player2.position;
+		Vector3 dir = player2.position - myTransform.position;
+		myTransform.position = Vector3.MoveTowards (myTransform.position, target, moveSpeed * Time.deltaTime);
+	}*/
 }
 
